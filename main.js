894b51d3bf279fde64700320336fcb26941be141
Andersonvin <andersonvin.eng@outlook.com>
2022-05-21 22:00:34 -0300

    Subindo arquivos do stage 04 do nlw return

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
 function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
@@ -6,7 +14,14 @@ function onScroll() {
   }
 }
 
- 
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
 function openMenu() {
   document.body.classList.add('menu-expanded')
 }
@@ -15,19 +30,17 @@ function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
 
-
-
-
 ScrollReveal({
-  origin:'bottom',
-  distance:'30px',
-  duration:1000,
+  origin: 'top',
+  distance: '30px',
+  duration: 700
 }).reveal(`
-#home,
-#home img, 
-#home .stats, 
-#services,
-#services header,
-#services .card,
-#about header,
-#about .content`);
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card
+  #about, 
+  #about header, 
+  #about .content`)
